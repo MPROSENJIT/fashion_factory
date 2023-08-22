7e6d5f6e431615d23fadcdb9e6a4f5886708c344
PROSENJIT <95131587+MPROSENJIT@users.noreply.github.com>
2023-08-22 22:54:20 +0530

    Add files via upload

---
+++ contact.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 20, 2023 at 08:28 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `birdprog`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact`
+--
+
+CREATE TABLE `contact` (
+  `Name` varchar(30) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `message` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `contact`
+--
+
+INSERT INTO `contact` (`Name`, `email`, `message`) VALUES
+('biswajit maity', 'biswajit@vedicrealty.com', 'test'),
+('biswajit maity', 'biswajit@vedicrealty.com', 'test'),
+('biswajit maity', 'biswajit@vedicrealty.com', 'test'),
+('biswajit maity', 'biswajit@vedicrealty.com', 'test'),
+('biswajit', 'biswajitmaity89497@gmail.com', 'test'),
+('biswajit', 'biswajitmaity89497@gmail.com', 'test');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
